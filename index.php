16ccc933900aa7f00369d2f611bbd18e718352bf
burkeda <davidburke@csus.edu>
2021-05-09 17:16:28 -0700

    Added index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- For Nunito Sans font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@1,200&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+    <div>
+        <div>
+            <a href="login.php" class="btn" role="button" aria->Login</a>
+        </div>
+    </div>
+
+</body>
+
+</html>
